e2e88339ba65ba0a518b8024db42081d5415b8ce
10190997 <petrocollege10190997@gmail.com>
2021-12-14 13:21:54 +0300

    Сессия 1

---
+++ Views/Pages/AddEditPage.xaml.cs
@@ -0,0 +1,172 @@
+﻿using Lopushok.Model;
+using Lopushok.Views.Windows;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Lopushok.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Product product { get; set; }
+
+        /// <summary>
+        /// Конструктор страницы
+        /// </summary>
+        /// <param name="product">Объект класса Продукт</param>
+        public AddEditPage(Product product)
+        {
+            InitializeComponent();
+            if (product != null)
+            {
+                this.product = product;
+            }
+            if (product.ID != 0)
+            {
+                btnDelete.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                btnDelete.Visibility = Visibility.Collapsed;
+            }
+            DataContext = product;
+            cbType.ItemsSource = DB.entities.ProductTypes.ToList();
+        }
+
+        /// <summary>
+        /// Показывает окно со всеми картинками в папке products
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnImage_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new ImagesWindow();
+            window.ShowDialog();
+
+            if (window.DialogResult == true)
+            {
+                product.Image = window.ImgUri;
+                // Обновить контекст, чтобы поменялась картинка
+                DataContext = null;
+                DataContext = product;
+            }
+        }
+
+        /// <summary>
+        /// Сохранить в БД с проверкой корректности данных
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            var errors = new StringBuilder();
+
+            if (string.IsNullOrEmpty(tbTitle.Text) || string.IsNullOrWhiteSpace(tbTitle.Text))
+            {
+                errors.AppendLine("Введите название");
+            }
+            if (string.IsNullOrEmpty(tbArticle.Text) || string.IsNullOrWhiteSpace(tbArticle.Text))
+            {
+                errors.AppendLine("Введите артикул");
+            }
+
+            else if (product.ID == 0)
+            {
+                var articles = DB.entities.Products.ToList();
+                foreach (var item in articles)
+                {
+                    if (tbArticle.Text == item.ArticleNumber)
+                    {
+                        errors.AppendLine("Артикул должен быть уникальным");
+                        break;
+                    }
+                }
+            }
+
+            if (string.IsNullOrEmpty(tbMinCostForAgent.Text) || string.IsNullOrWhiteSpace(tbMinCostForAgent.Text))
+            {
+                errors.AppendLine("Введите минимальную стоимость для агента");
+            }
+            else
+            {
+                if (!decimal.TryParse(tbMinCostForAgent.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out _))
+                {
+                    errors.AppendLine("Введите верную стоимость");
+                }
+            }
+            if (!string.IsNullOrEmpty(tbPersonCount.Text))
+            {
+                if (!int.TryParse(tbPersonCount.Text, out _))
+                {
+                    errors.AppendLine("Введите корректное количество человек");
+                }
+            }
+            if (!string.IsNullOrEmpty(tbWorkshopNumber.Text))
+            {
+                if (!int.TryParse(tbWorkshopNumber.Text, out _))
+                {
+                    errors.AppendLine("Введите корректный номер роизводственного цеха");
+                }
+            }
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (product.ID == 0)
+            {
+                DB.entities.Products.Add(product);
+            }
+            DB.entities.SaveChanges();
+            MessageBox.Show("Успешно сохранено");
+            NavigationService.GoBack();
+        }
+
+        /// <summary>
+        /// Удалить из БД
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Вы уверены?", "Подтверждение", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                if (product.ProductSales.Count > 0)
+                {
+                    MessageBox.Show("У продукта есть информация о его продажах агентами. Удаление запрещено.");
+                    return;
+                }
+                // Если у продукта есть информация о материалах, используемых при его производстве,
+                // или история изменения цен, то эта информация должна быть удалена вместе с продуктом.
+
+                if (product.MaterialsList != null)
+                {
+                    var pms = product.ProductMaterials.ToList();
+                    foreach (var item in pms)
+                    {
+                        DB.entities.ProductMaterials.Remove(item);
+                    }
+                }
+                if (product.ProductCostHistories.Count != 0)
+                {
+                    var pchs = product.ProductCostHistories.ToList();
+                    foreach (var item in pchs)
+                    {
+                        DB.entities.ProductCostHistories.Remove(item);
+                    }
+                }
+                DB.entities.Products.Remove(product);
+                DB.entities.SaveChanges();
+                MessageBox.Show("Успешно удалено");
+                NavigationService.GoBack();
+            }
+        }
+    }
+}
---
+++ Views/Windows/ImagesWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace Lopushok.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ImagesWindow.xaml
+    /// </summary>
+    public partial class ImagesWindow : Window
+    {
+        private string _imgUri;
+
+        public string ImgUri
+        {
+            get
+            {
+                return _imgUri;
+            }
+        }
+
+        /// <summary>
+        /// Конструктор окна
+        /// </summary>
+        public ImagesWindow()
+        {
+            InitializeComponent();
+            ShowImages();
+        }
+
+        /// <summary>
+        /// Получает изображения из папки products
+        /// </summary>
+        /// <returns>Массив ссылок на изображения</returns>
+        private string[] GetImages()
+        {
+            string[] files = Directory.GetFiles("../../products");
+            for (int i = 0; i < files.Length; i++)
+            {
+                files[i] = files[i].Remove(0, 5);
+            }
+            return files;
+        }
+
+        /// <summary>
+        /// Выводит StackPanel с изображениями
+        /// </summary>
+        private void ShowImages()
+        {
+            const int imagesInRow = 5;
+
+            var files = GetImages();
+
+            var column = new StackPanel();
+            column.Orientation = Orientation.Vertical;
+            column.HorizontalAlignment = HorizontalAlignment.Center;
+            column.VerticalAlignment = VerticalAlignment.Center;
+
+            for (int i = 0; i <= files.Length / imagesInRow; i++)
+            {
+                var row = new StackPanel();
+                row.Orientation = Orientation.Horizontal;
+                row.Margin = new Thickness(0, 5, 0, 5);
+                for (int j = imagesInRow * i; j < imagesInRow * i + imagesInRow; j++)
+                {
+                    if (j == files.Length)
+                    {
+                        break;
+                    }
+                    var image = new Image();
+                    image.Width = 60;
+                    image.Height = 60;
+                    image.Margin = new Thickness(5, 0, 5, 0);
+                    image.Source = new BitmapImage(new Uri(files[j], UriKind.Relative));
+                    image.Cursor = Cursors.Hand;
+                    image.Tag = files[j];
+                    image.MouseLeftButtonDown += Image_MouseLeftButtonDown;
+                    row.Children.Add(image);
+                }
+                column.Children.Add(row);
+            }
+            gridImages.Children.Add(column);
+        }
+
+        /// <summary>
+        /// Выбор картинки
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var image = (Image)sender;
+            _imgUri = image.Tag.ToString();
+            DialogResult = true;
+            Close();
+        }
+    }
+}
